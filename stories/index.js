6233e3267b145b42c37329fac6451e3c3940f25e
Ryan Zhen <rjhzhen@gmail.com>
2022-12-01 22:10:41 -0500

    Initial commit, button states working

--- stories/index.js
+++ stories/index.js
@@ -12,13 +12,8 @@ storiesOf("Button", module)
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
   })
   .add("Base", () => <Button>Base</Button>)
-  .add("Confirm", () => <Button confirm>Confirm</Button>)
-  .add("Danger", () => <Button danger>Cancel</Button>)
-  .add("Clickable", () => (
-    <Button onClick={action("button-clicked")}>Clickable</Button>
-  ))
-  .add("Disabled", () => (
-    <Button disabled onClick={action("button-clicked")}>
-      Disabled
-    </Button>
+  .add("Confirm", () => <Button class="button button--confirm">Confirm</Button>)
+  .add("Danger", () => <Button class="button button--danger">Danger</Button>)
+  .add("Clickable", () => (<Button onClick={action("button-clicked")}>Clickable</Button>))
+  .add("Disabled", () => (<Button disabled onClick={action("button-clicked")}>Disabled</Button>
   ));
