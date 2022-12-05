51b1590dceb623e1a6b9d7b01e28a9e5c35bf1c4
Ryan Zhen <rjhzhen@gmail.com>
2022-12-05 00:04:35 -0500

    Implementation of interviewer components

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem (props) {
+  const {name, avatar, selected, setInterviewer} = {...props};
+  const interviewerClass = classNames("Interviewers__item", {
+    "interviewers__item--selected" : selected
+  })
+  return (
+    <li className={interviewerClass} onClick={setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  )
+}
