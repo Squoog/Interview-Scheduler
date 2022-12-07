302919e9eb4165dcd654ebcccd4572b578997672
Ryan Zhen <rjhzhen@gmail.com>
2022-12-07 01:20:55 -0500

    Data transformation using selectors, change to state management

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Appointment";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import axios from "axios";
 
 const appointments = {
   "1": {
@@ -46,18 +46,22 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState(["Monday"]);
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState( prev => ({... prev, days}));
 
   useEffect(()=>{
     const URL = `http://localhost:8001/api/days`;
     axios.get(URL).then(response => {
-      console.log(URL)
-      console.log(response.data);
-      setDays([...response.data]);
+      setDays(response.data);
     })
-  }, [days])
-  
+  }, [])
+
   const schedule = Object.values(appointments).map(appointment => {
     return(
       <Appointment
@@ -77,8 +81,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         />
       </nav>
