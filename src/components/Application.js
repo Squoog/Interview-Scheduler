550f4cff8ac1998e427d0aa311201e87f7d77e95
Ryan Zhen <rjhzhen@gmail.com>
2022-12-07 02:09:47 -0500

    Retrieve interviewer data through API

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,13 +6,14 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-import { getAppointmentsForDay } from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
@@ -22,19 +23,24 @@ export default function Application(props) {
   useEffect(()=>{
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments")
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
     ]).then((all) => {
       console.log(all[0].data, all[1].data);
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
 
     });
   }, [])
 
   const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
     return(
       <Appointment
       key={appointment.id}
-      {...appointment}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
     />)
   })
 
